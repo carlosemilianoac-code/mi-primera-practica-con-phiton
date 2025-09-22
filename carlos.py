e72fe0aa8d441dee50278e30b0f5b4736a10ae64
carlosemilianoac-code <carlosemilianoac@isb.edu.mx>
2025-09-22 14:52:13 +0000

    se guardan cambios

--- carlos.py
+++ carlos.py
@@ -9,3 +9,11 @@ edad=int(input)("ingresa tu edad")
 pasatiempo=input("igresa tu pasatiempo:") 
 materia=input("ingresa materia:") 
 print("ahora vamos a calcular cuantos días faltan para tu cumple🎂")
+dia=int(input("Díade tu cumpleaños? ()")1-31):"))
+
+mes=int(input("¿mes de tu cumpleaños? (1-12):"))
+hoy = date.today()
+cumple=date(hoy.year,mes,dia)
+if cumple<hoy:cumple= date(hoy.year + 1,mes, dia) 
+dias = (cumple-hoy).days
+print(f"Hola{nombre}🏅,
