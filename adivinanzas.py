c131d2b5140cdd45079c0ebe5b30376b1d452964
carlosemilianoac-code <carlosemilianoac@isb.edu.mx>
2025-10-13 15:00:58 +0000

    "Se guarda práctica"

---
+++ adivinanzas.py
@@ -0,0 +1,13 @@
+import random
+
+#paso 1:crear la lista de adivinanzas (preguntas, respuetas)
+
+adivinanzas=[
+    ("blanco por dentro,verde por fuea.si quieres que te lo diga.espera.","pera")
+    ("oro parece,plato no es.¿que es?","platano"),
+    ("tengo agujas pero no coso,tengo numeros pero no soy matemaricas.¿soy?","reloj"),
+    ("tiene dientes pero no merde.¿que es?","peine"),
+    ("cuanto mas seca,mas moja, ¿que   es ?","toalla"),
+    ("tine cuello pero no cabeza .que es?","botella"),
+    ("tiene hoja pero no")
+]
